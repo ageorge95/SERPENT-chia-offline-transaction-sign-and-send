f753da47d0817d35d4543c0bf242048c46e442c9
George Daniel Arteni <arteni.george.daniel@gmail.com>
2021-12-19 13:15:44 +0200

    added the CLI to build-windows.yml

--- _00_update_version.py
+++ _00_update_version.py
@@ -1,5 +1,9 @@
 import sys
 
 open("version.txt", "w").write(sys.argv[-1])
+
 spec_replacement=str(open("_00_GUI.spec", "r").readlines()).replace(r'name=''\'_00_GUI\'''', r'name=''\'{}\''''.format(sys.argv[-1]))
 open("_00_GUI.spec", "w").writelines(eval(spec_replacement))
+
+spec_replacement=str(open("_00_CLI.spec", "r").readlines()).replace(r'name=''\'_00_CLI\'''', r'name=''\'{}\''''.format(sys.argv[-1]))
+open("_00_CLI.spec", "w").writelines(eval(spec_replacement))
