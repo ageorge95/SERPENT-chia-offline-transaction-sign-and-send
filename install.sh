2f1252515089ee435ee5e6eac292c9a5f7c55a68
ageorge95 <arteni.george.daniel@gmail.com>
2023-01-21 20:33:51 +0200

    reworked install.sh

--- install.sh
+++ install.sh
@@ -1,66 +1,37 @@
+root=$(pwd)
+
 # exit script when an error is detected
 set -o errexit
 
-# help message
-
-HELP="\
-Usage: $0 [-sh]
-
-    -s    Upgrade setuptools in SERPENT's Python virtual environment. Use this
-          if the installation isn't working without it.
-    -h    Display this help message and exits.
-"
-
-help()
-{
-    echo "$HELP"
-}
-
-# argument handling
-
-while getopts sh flag
-do
-    case "${flag}" in
-        s) SETUPTOOLS=setuptools;;
-        h) help; exit 0;;
-        *) help; exit 1;;
-    esac
-done
-
 # installation
-
-PACKAGES='python3.7-minimal python3-venv python3.7-venv'
-
-# check if $PACKAGES are already installed
-dpkg -s $PACKAGES
-
-# if they aren't already installed...
-if [ $? -ne 0 ]
-then
-    sudo apt update
-    sudo apt install $PACKAGES
-fi
-
-git submodule update --init
+git submodule update --progress --init --recursive --force
 ln -s chia_blockchain/chia .
 
-python3.7 -m venv .
-ln -s bin/activate .
+python3 -m venv venv
+ln -s venv/bin/activate .
+ln -s $(pwd)/chia_blockchain/chia venv/bin/
+ln -s $(pwd)/src venv/bin/
+ln -s $(pwd)/media venv/bin/
+
 . ./activate
 
-pip3 install -U pip $SETUPTOOLS
-pip3 install -r requirements.txt
-
-python setup.py develop
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+python3 setup.py install
 
 # post-installation message
-
 echo "
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 SERPENT install.sh complete.
 Join the Discord server for support: https://discord.gg/yPdCmHSgMe
 
-Run '. ./bin/activate' to activate SERPENT's Python virtual environment and
+Run '. activate' to activate SERPENT's Python virtual environment and
 'deactivate' to, well, deactivate it.
 
 Run 'serpent -h' for further instructions.
+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 "
+
+rm -r $root/build
+rm -r $root/dist
+rm -r $root/SERPENT.egg-info
